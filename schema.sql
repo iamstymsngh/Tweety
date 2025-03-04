c126ef842c7e0efacfa4179b91e52ad6c99d9494
iamstymsngh <iamstymsingh@gmail.com>
2025-03-04 12:51:16 +0530

    Fixing schema issues

--- schema.sql
+++ schema.sql
@@ -4,8 +4,8 @@ CREATE DATABASE IF NOT EXISTS tweety;
 SET DATABASE = tweety;
 
 -- Create USERS table
-CREATE TABLE Users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL NOT NULL PRIMARY KEY,
     email varchar NOT NULL UNIQUE,
-    username varchar NOT NULL UNIQUE,
+    username varchar NOT NULL UNIQUE
 );
